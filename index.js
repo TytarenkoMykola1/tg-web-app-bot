e0d6c0015d2712033f836a4c91adef19ee9d4d55
TytarenkoMykola1 <mykola.tytarenko-ki221k@nung.edu.ua>
2024-03-20 20:33:28 +0200

    add controler

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
-
+const express = require('express');
+const cors = require('cors');
 const token = '7124125013:AAEPqBpQBRaq4rRekeE3Vcucaxu28DvC3dQ';
-const webAppUrl = 'https://silver-macaron-d5de91.netlify.app/';
+const webAppUrl = 'https://silver-macaron-d5de91.netlify.app';
 const bot = new TelegramBot(token, {polling: true});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -25,5 +31,45 @@ bot.on('message', async (msg) => {
             }
         })
     }
+    if(msg?.web_app_data?.data) {
+        try{
+            const data = JSON.parse(msg?.web_app_data?.data)
 
+           await bot.sendMessage(chatId,'Дякую за зворотній відгук!')
+           await bot.sendMessage(chatId,'Ваша країна: ' + data?.country);
+           await bot.sendMessage(chatId,'Ваша вулиця: ' + data?.street);
+
+           setTimeout(async () => {
+               await bot.sendMessage(chatId,'Всю інформацію ви получите в цьому чаті');
+           }, 3000)
+        } catch (e){
+            console.log(e);
+        }
+        const data = JSON.parse(msg?.web_app_data?.data)
+    }
 });
+
+app.post('/web-data', async (req, res) => {
+    const {queryId, products, totalPrice} = req.body;
+    try {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Успішна покупка',
+            input_message_content: {message_text: 'Вітаю з покупкою, ви купити товар на суму ' + totalPrice}
+        })
+        return res.status(200).json({});
+    } catch (e) {
+        await bot.answerWebAppQuery(queryId, {
+            type: 'article',
+            id: queryId,
+            title: 'Не вдалось купити товар ',
+            input_message_content: {message_text: 'Не вдалось купити товар '}
+        })
+    }
+    return res.status(500).json({});
+})
+
+const PORT = 8000;
+
+app.listen(PORT,() => console.log('server started on PORT' + PORT))
